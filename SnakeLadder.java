162cfe12743f7f5fd24f550fec8c96fb659ccd03
ajayjagtap-1404 <ajayjagtap1404@gmail.com>
2023-02-25 00:03:50 +0530

    Adding : code for rolling die and check case play or not play

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -5,9 +5,31 @@ public class SnakeLadder {
         int position = 0;
         System.out.println("Player is at Position " + position);
 
-        // adding random function for dice number(1 to 6)
         int randomDiceNumber = (int) Math.floor(Math.random() * 100) % 6 + 1;
 
-        System.out.println("Dice number is " + randomDiceNumber);
+        // use for get options
+        int options = (int) Math.floor(Math.random() * 10) % 3;
+
+        switch (options) {
+            case 1:
+                // snake
+                position = position - randomDiceNumber;
+                if (position < 0) {
+                    position = 0;
+                }
+                break;
+            case 2:
+                // ladder
+                // increasing position of player by adding randomDiceNumber variable
+                position =  position + randomDiceNumber;
+                break;
+            default:
+                // no play
+                // position remains same
+                position = position;
+                break;
+        }
+
+        System.out.println("final Position " + position);
     }
 }
