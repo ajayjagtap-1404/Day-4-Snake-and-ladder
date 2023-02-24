b143c6c70340e4e7de0179e0f10c2a599886887c
ajayjagtap-1404 <ajayjagtap1404@gmail.com>
2023-02-25 00:11:12 +0530

    Adding : Code for player reaches position 100

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -2,34 +2,37 @@ package com.snake_ladder;
 
 public class SnakeLadder {
     public static void main(String[] args) {
+        System.out.println("Welcome to Snake Ladder program :");
         int position = 0;
-        System.out.println("Player is at Position " + position);
 
-        int randomDiceNumber = (int) Math.floor(Math.random() * 100) % 6 + 1;
+        for (position = 0; position < 100 ; position++){
 
-        // use for get options
-        int options = (int) Math.floor(Math.random() * 10) % 3;
+            int randomDiceNumber = (int) Math.floor(Math.random() * 100 ) % 6 + 1 ;
+            int option = (int) Math.floor(Math.random() * 10) % 3;
 
-        switch (options) {
-            case 1:
-                // snake
-                position = position - randomDiceNumber;
-                if (position < 0) {
-                    position = 0;
-                }
-                break;
-            case 2:
-                // ladder
-                // increasing position of player by adding randomDiceNumber variable
-                position =  position + randomDiceNumber;
-                break;
-            default:
-                // no play
-                // position remains same
-                position = position;
-                break;
+            switch (option) {
+                case 1:
+                    // snake
+                    position = position - randomDiceNumber;
+                    if (position < 0) {
+                        position = 0;
+                    }
+                    break;
+                case 2:
+                    // ladder
+                    // increasing position of player by adding randomDiceNumber variable
+                    position =  position + randomDiceNumber;
+
+                    break;
+                default:
+                    // no play
+                    // position remains same
+                    position = position;
+                    break;
+            }
+
         }
+        System.out.print("final Position " + position + " ");
 
-        System.out.println("final Position " + position);
     }
 }
